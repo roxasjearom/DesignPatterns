a1ebc174d1a8e33990a2c6384c63605c1bce8bcb
Jearom Roxas <roxasjearom@gmail.com>
2025-03-16 15:10:59 +0800

    Update AGP to 8.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.roxasjearom.designpatterns"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.roxasjearom.designpatterns"
